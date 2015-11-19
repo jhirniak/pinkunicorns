04a49903685e6e124697ad2b128070d7b5588221
Michael Lotkowski <michael@lotkowski.co.uk>
2015-11-19 15:17:31 -0800

    keywords and categories detection

--- server/app/nlp/nlp.py
+++ server/app/nlp/nlp.py
@@ -1,3 +1,19 @@
 from alchemyapi import AlchemyAPI
 
-def get_category
+
+alchemyapi = AlchemyAPI()
+
+def get_categories(text):
+    response = alchemyapi.taxonomy('text', text)
+
+    if response['status'] == 'OK' and len(response['taxonomy']) > 0:
+        taxonomy = response['taxonomy'][0]
+        tokens = taxonomy['label'].split('/')
+        return tokens[1]
+
+
+def get_keywords(text):
+    response = alchemyapi.keywords('text', text)
+
+    if response['status'] == 'OK' and len(response['keywords']) > 0:
+        return [x['text'] for x in response['keywords']]
