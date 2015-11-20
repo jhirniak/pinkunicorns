c785654d68c210511c3529c2232201d3fe469ebd
Jaroslaw Hirniak <j@hirniak.info>
2015-11-19 17:17:10 -0800

    Auth up to now. Will work on celery.

--- server/app/app.py
+++ server/app/app.py
@@ -1,10 +1,12 @@
-import auth
-
 from flask import Flask
+from flask import Blueprint, render_template, abort
 from flask.ext.restful import Api
 
 from extensions import bootstrap, db
 from settings.config import config
+
+from social.apps.flask_app.default.models import init_social
+
 
 def create_app(config_name):
     """Create a flask app from a config"""
@@ -22,9 +24,31 @@ def create_app(config_name):
 
     return app
 
+
 def register_extensions(app):
     """Register flask extensions"""
     db.init_app(app)
     bootstrap.init_app(app)
+    init_social(app, db)
 
 
+@app.before_request
+def global_user():
+    g.user = login.current_user
+
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return User.query.get(int(userid))
+    except (TypeError, ValueError):
+        pass
+
+
+# Make current user available on templates
+@app.context_processor
+def inject_user():
+    try:
+        return {'user': g.user}
+    except AttributeError:
+        return {'user': None}
--- server/settings/config.py
+++ server/settings/config.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SOCIAL_AUTH_USER_MODEL = 'app.main.models.User'
 
     @staticmethod
     def init_app(app):
