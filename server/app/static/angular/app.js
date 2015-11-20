024c92d544fd229ad3466d72571a809b96c6fd5b
Bartosz Krol <adrilan.evenwood@gmail.com>
2015-11-20 07:24:08 -0800

    Let it go, let it go

--- server/app/static/angular/app.js
+++ server/app/static/angular/app.js
@@ -57,22 +57,4 @@ app.controller("UnicornCtrl", ['$scope', '$http', '$sce', function($scope,$http,
 
     console.log('Controller ready');
 
-    var bc = this;
-    bc.isLoaded = false;
-    bc.likes = [];
-    bc.name = '';
-    bc.products = {};
-
-    bc.stuff = function () {
-        $http({
-            method: 'GET',
-            url: '/api/v1/jarvis?text=What+should+I+get+Konrad+for+his+birthday'
-        }).then(function (response) {
-            bc.likes = Object.keys(response.data.products);
-            bc.name = response.data.name;
-            bc.products = _.flatten(_.values(response.data.products));
-            console.log(response);
-            bc.isLoaded = true;
-        });
-    }
 }]);
