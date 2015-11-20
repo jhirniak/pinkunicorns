b95f3017ab367aba7480f6325c76cc8ec0729dae
Konrad Stojanowski <konrad@stojanowski.eu>
2015-11-20 12:33:46 -0800

    some changes

--- server/app/api/opentable.py
+++ server/app/api/opentable.py
@@ -4,12 +4,19 @@ API_URL = 'http://opentable.herokuapp.com/api/'
 
 
 def get_restaurants(city, address, zip, country=None):
+
+    if city.has_key('location'):
+        city = city['location'][0]['value'];
+
     params = {
         'country': country or 'US',
     }
 
     if city:
         params['city'] = city
+
+    if city.strip() == "near me":
+        params['city'] = "1 Hacker Way, Menlo Park"
 
     if zip:
         params['zip'] = zip
