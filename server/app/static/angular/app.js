5a523405f6a4088cf8f5ed5b0daccc09ea44319c
Konrad Stojanowski <konrad@stojanowski.eu>
2015-11-20 04:26:37 -0800

    kupa

--- server/app/static/angular/app.js
+++ server/app/static/angular/app.js
@@ -5,4 +5,16 @@ app.controller("unicornCtrl", function($scope) {
     $scope.left  = function() {return 100 - $scope.message.length;};
     $scope.clear = function() {$scope.message = "";};
     $scope.save  = function() {alert("Note Saved");};
+    $scope.get_query = function() {
+    	$.get('/api/v1/jarvis?text=' + $scope.query, function (d) {
+	      $('#herestuff').html(d);
+	      console.log(d);
+	    });
+    }
+
+    $scope.$watch("query", function(newValue, oldValue) {
+    if (newValue != oldValue) {
+      get_query();
+    }
+  });
 });
