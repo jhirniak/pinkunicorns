387c6c3e1242369ff6a985cf59ca1947ad4b33f8
Konrad Stojanowski <konrad@stojanowski.eu>
2015-11-20 04:33:06 -0800

    kupa

--- server/app/static/angular/app.js
+++ server/app/static/angular/app.js
@@ -2,6 +2,7 @@ var app = angular.module("unicornX", []);
 
 app.controller("unicornCtrl", function($scope) {
     $scope.message = "";
+    $scope.query = "";
     $scope.left  = function() {return 100 - $scope.message.length;};
     $scope.clear = function() {$scope.message = "";};
     $scope.save  = function() {alert("Note Saved");};
