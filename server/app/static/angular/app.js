2e4f29f2ac58b877bbe64c37223748f582a148ea
Konrad Stojanowski <konrad@stojanowski.eu>
2015-11-20 07:14:34 -0800

    .

--- server/app/static/angular/app.js
+++ server/app/static/angular/app.js
@@ -1,12 +1,15 @@
 var app = angular.module('unicornX', []);
 
-app.controller("UnicornCtrl", ['$scope', '$http', function($scope) {
+app.controller("UnicornCtrl", ['$scope', '$http', '$sce', function($scope,$http,$sce) {
     $scope.visibility = "";
 
     $scope.isVisible = function(x) {
     	return $scope.visibility == x;
     }
 
+    //$sce.trustAsResourceUrl("https://maps.googleapis.com/maps/api/directions/json");
+
+    
     $scope.message = "";
     $scope.query = "";
 
@@ -17,10 +20,12 @@ app.controller("UnicornCtrl", ['$scope', '$http', function($scope) {
     $scope.get_query = function() {
     	$.get('/api/v1/jarvis?text=' + $scope.query + '&access_token='+window.authtoken, function (d) {
 	      console.log(d);
+	      alert(d["type"]);
 	      if(d["type"] == "travel") {
 	      	$scope.accomodation = d["accomodation"];
 	      	$scope.travel = d["travel"];
 	      	$scope.visibility = "travel";
+	      	$scope.getRU = $sce.trustAsResourceUrl('https://maps.googleapis.com/maps/api/directions/json?origin='+$scope.travel['places'][0]['pos']+'&destination='+$scope.travel['places'][1]['pos']+'&key=AIzaSyAeLkV7n7z_Kt44uryKbPWuJ7ISHweKBqM');
 	      } else if(d["type"] == "birthdays") {
 	      	$scope.likes = Object.keys(d.products);
             $scope.name = d.name;
